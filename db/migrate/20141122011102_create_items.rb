e8c661bd028d1bc035b114bb28fa234dc8cd599a
Katharine Chen <katharinechen.ny@gmail.com>
2014-11-21 17:14:04 -0800

    Add item schema to db

---
+++ db/migrate/20141122011102_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.string :name 
+      t.integer :PAR
+      t.integer :current_stock_level 
+      t.timestamps 
+    end
+  end
+end 
+
