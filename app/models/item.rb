2969c5f1ae886cd74fd86ff12dd2340a48cac1ba
Katharine Chen <katharinechen.ny@gmail.com>
2014-11-22 22:00:13 -0800

    Add difference col to table

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,8 @@ class Item < ActiveRecord::Base
   validates :PAR, presence: true, numericality: { only_integer: true }
   validates :current_stock_level, presence: true, numericality: { only_integer: true }
   
+  def difference
+    return self.PAR - self.current_stock_level
+  end 
+
 end 
